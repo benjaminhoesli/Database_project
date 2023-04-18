ec0b93bfbac4d303dcae07f5bb6826c25254b1d8
PeterG18 <81383703+PeterG18@users.noreply.github.com>
2023-04-18 15:02:02 -0400

    Update project.sql

--- project.sql
+++ project.sql
@@ -305,7 +305,27 @@ WHERE StudentID IN (
  JOIN GRADE_WEIGHT ON GRADE_WEIGHT.GradeWeightID = ASSIGNMENTS.GradeWeightID
  JOIN COURSES ON COURSES.CourseID = GRADE_WEIGHT.CourseID
  WHERE GRADES.StudentID = 150 AND COURSES.CourseID = 85675;
-
  
+--Task 12
+WITH graded_assignments AS (
+  SELECT g.StudentID, gw.Category, gw.Weight, g.Grade, 
+         ROW_NUMBER() OVER (
+           PARTITION BY gw.CourseID, gw.Category 
+           ORDER BY g.Grade DESC
+         ) AS rank
+  FROM GRADES g
+  JOIN ASSIGNMENTS a ON g.AssignmentID = a.AssignmentID
+  JOIN GRADE_WEIGHT gw ON a.GradeWeightID = gw.GradeWeightID
+  WHERE g.StudentID = 150
+),
+dropped_lowest AS (
+  SELECT StudentID, Category, Weight, AVG(Grade) AS avg_grade
+  FROM graded_assignments
+  WHERE rank > 1
+  GROUP BY StudentID, Category, Weight
+)
+SELECT SUM(Weight * avg_grade) / SUM(Weight) AS overall_grade
+FROM dropped_lowest;
+
  
 
